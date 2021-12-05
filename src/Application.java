58c8bb2aaf8b56643d80fec2cfb4ad1f9ed65335
monty Defreitas <Monty.Defreitas@hotmail.com>
2021-12-05 13:34:16 -0600

    Changed game output format

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,23 @@
 public class Application {
 
-  static String winner = " ";
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  {
         Deck deck = new Deck();
         System.out.println("Making the deck...");
         Player draws = new Player("Draws");
-        Player one = new Player("Player1");
-        Player two = new Player("Player2");
+        Player one = new Player("Monty");
+        Player two = new Player("Eli");
 
+        String p1P2 = "\t" + "\t" + "[" + one.name + "] Wins";
 
-
+        StringBuilder builder2 = new StringBuilder();
+        for(int i=0;i<p1P2.length() * 2;i++){
+            builder2.append(" ");
+        }
+        StringBuilder builder3 = new StringBuilder();
+        for(int i=0;i<7 * 2;i++){
+            builder3.append(" ");
+        }
 
         System.out.println("Shuffling the Deck...");
         deck.shuffle();
@@ -21,24 +29,29 @@ public class Application {
 
         for (int i = 0; i < 26; i++){
 
-            System.out.println(one.name + " Draws: " + one.getCardValue() + " of " + one.getCardSuit());
+            System.out.print(one.name + " Drew: " + one.getCardValue() + " of " + one.getCardSuit());
             int oneNum = one.flip().getNumber();
-            System.out.print("\t" + two.name + " Draws: " + one.getCardValue() + " of " + one.getCardSuit() + "\n");
+            System.out.print("<--->");
+            System.out.print(two.name + " Drew: " + one.getCardValue() + " of " + one.getCardSuit() + "\n");
             int twoNum = two.flip().getNumber();
+
 
             if (oneNum > twoNum){
                 one.score ++;
 
-                System.out.println("\t" + "\t" + "[" + one.name + "] Wins");
+                System.out.println("[" + one.name + "] Wins");
 
             }  else if (twoNum > oneNum) {
                 two.score ++;
 
-                System.out.println("\t" + "\t" +"[" + two.name + "] Wins");
+                System.out.println(builder2 + "[" + two.name + "] Wins");
+
             } else {
+                System.out.println( builder3 + "[Draw, Fight harder!]");
                draws.score++;
             }
         }
+
         System.out.println(one.name + " with " + one.score + " wins");
         System.out.println(two.name + " with " + two.score +  " wins");
         System.out.println("Times tied: " + draws.score);
