0324351822aba6a9a2a0b0cbd9b48e0bdb3328a6
monty Defreitas <Monty.Defreitas@hotmail.com>
2021-12-09 22:38:20 -0600

    moved cardvalue and suit into constructor.

--- src/Deck.java
+++ src/Deck.java
@@ -5,24 +5,23 @@ import java.util.List;
 
 public class Deck {
 
-    private static final List<String> cardValue = List.of("2", "3", "4", "5", "6",
-            "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
 
-    private static final List<String> suitNames = List.of("Clubs", "Diamonds", "Hearts", "Spades");
-
-
-    private static final List<Card> cards = new ArrayList<Card>();
+    private final  List<Card> cards = new ArrayList<Card>();
 
     public Deck() {
 
+        List<String> cardValue = List.of("2", "3", "4", "5", "6",
+                "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
         for (int i = 0; i < cardValue.size(); i++) {
             String value = cardValue.get(i);
             int number = i + 2;
+            List<String> suitNames = List.of("Clubs", "Diamonds", "Hearts", "Spades");
             for (String suitName : suitNames) {
                 cards.add(new Card(value, suitName, number));
             }
         }
     }
+
 
     public void shuffle() {
         Collections.shuffle(cards);
